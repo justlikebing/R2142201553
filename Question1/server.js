9e45a6b22687111c190060bd6ef8f5dbcc17bae5
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-07-01 10:37:36 +0530

    Update server.js

--- Question1/server.js
+++ Question1/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
-
 // Replace with your actual Authorization Token
 const authToken = 'YOUR_AUTH_TOKEN';
 
